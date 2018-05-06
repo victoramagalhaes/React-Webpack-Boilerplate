894ee153fc5ba30e2462e0c1f48881ba64913910
Victor Magalhães <contatovictormagalhaes@hotmail.com>
2018-05-05 22:13:32 -0300

    Corrigindo alguns detalhes

--- src/index.jsx
+++ src/index.jsx
@@ -2,4 +2,4 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import "./scss/main.scss"
 
-ReactDOM.render(<h1>React</h1>, document.getElementById('app'))
+ReactDOM.render(<h1>React Funcionando</h1>, document.getElementById('app'))
